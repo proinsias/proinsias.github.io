a39fada65344982413b89a60c4e8e5317637dfb3
Francis O'Donovan <francis.odonovan@gmail.com>
2018-10-12 15:11:20 +0000

    Deploy from Forestry.io

--- _posts/2016-06-22-hello-world.md
+++
@@ -1,9 +0,0 @@
----
-title: "Hello World!"
-date: 2016-06-20
-modified: 2016-06-21
-layout: single
----
-
-In the proud tradition of programmers everywhere, I use my first blog post to say "Hello World!".
-
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,101 @@
+var store = [{
+        "title": "The Little Telescope that Could",
+        "excerpt":"This article was originally published in The Big Eye Newsletter (Vol 1 No 2, Spring 2006). A new visitor to the Palomar Observatory would be forgiven for paying scant attention to the small hut-like structure shown in Figure 1. A few minutes walk from the gigantic dome of the five-meter...","categories": ["tres"],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/tres/the-little-telescope-that-could/",
+        "teaser":null},{
+        "title": "Minnow Telescope Finds Massive Planet",
+        "excerpt":"This article was originally published in the Astronomy Ireland Magazine (March 2007). Since ancient times, mankind has studied the sky and wondered what those “wandering stars” (the solar system planets) might be. Centuries of observations of these celestial bodies led to the realization that Earth was not the center of...","categories": ["tres"],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/tres/minnow-telescope-finds-massive-planet/",
+        "teaser":null},{
+        "title": "Hello World!",
+        "excerpt":"In the proud tradition of programmers everywhere, I use my first blog post to say “Hello World!”.   ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/hello-world/",
+        "teaser":null},{
+        "title": "The Friendliest of Fire",
+        "excerpt":"How on earth could an American pilot get a Distinguished Flying Cross for shooting down an American plane? The Friendliest of Fire (via Now I Know) tells all.  ","categories": ["links"],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/links/The-Friendliest-of-Fire-Now-I-Know/",
+        "teaser":null},{
+        "title": "crontab.guru - the cron schedule expression editor",
+        "excerpt":"Do you always forget how to read/write cronjobs? Check out crontab.guru - the cron schedule expression editor.   ","categories": ["links"],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/links/crontab-guru-the-cron-schedule-expression-editor/",
+        "teaser":null},{
+        "title": "The relativity of raw data",
+        "excerpt":"Data scientists often say that they want access to the ‘raw data’ – but what does that term mean? An important characteristics of raw data [is that it] is relative to your reference frame. The raw data is raw to you if you have done no processing, manipulation, coding, or analysis...","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/The-relativity-of-raw-data-Simply-Statistics/",
+        "teaser":null},{
+        "title": "Making machine learning models interpretable",
+        "excerpt":"This month, the 20th European Symposium on Artificial Neural Networks, Computational Intelligence and Machine Learning held a special session on “Interpretable Models in Machine Learning”. Here’s the introductory paper for the session.   ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Interpretable-Models-in-Machine-Learning/",
+        "teaser":null},{
+        "title": "Death by Diagnosis on Freakonomics",
+        "excerpt":"Q: What’s the number-one problem in healthcare? A: I think the number-one problem is we don’t measure performance. We don’t measure the outcomes of patients in health care for 99 percent of the health care that’s delivered. (via Freakonomics) This was a perfect listen for me, given my new position...","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Death-by-Diagnosis-on-Freakonomics/",
+        "teaser":null},{
+        "title": "The Data Visualisation Catalogue",
+        "excerpt":"Looking for inspiration for your data viz project? Can’t remember what a particular visualization is called? Check out the Data Visualisation Catalogue.   ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/The-Data-Visualisation-Catalogue/",
+        "teaser":null},{
+        "title": "Why Should I Trust You?: Explaining the Predictions of Any Classifier",
+        "excerpt":"If you can’t understand how a model makes a prediction, how can you trust that prediction? LIME [is] a novel explanation technique that explains the predictions of any classifier in an interpretable and faithful manner, by learning an interpretable model locally around the prediction. (via Why Should I Trust You?:...","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Why-Should-I-Trust-You-Explaining-the-Predictions-of-Any-Classifier/",
+        "teaser":null},{
+        "title": "The Truth About Bad Science",
+        "excerpt":"This wired.com article on ‘bad science’ speaks to me on so many levels. It hurts my soul to see how many published studies are not reproducible.      “A new study shows…” are “the four most dangerous words”.   ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/The-Truth-About-Bad-Science/",
+        "teaser":null},{
+        "title": "The legends of mathematics that almost never were",
+        "excerpt":"It’s always made me sad when people tell me they dislike mathematics. I always wonder if it was the subject or their teachers that they disliked… Mathematical genius resides within every one of us. Most people just don’t know it yet. That’s because genius is fragile. If you don’t embraced...","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/The-legends-of-mathematics-that-almost-never-were/",
+        "teaser":null},{
+        "title": "Unlearning descriptive statistics",
+        "excerpt":"Top tips on better descriptive statistics: Instead of the mean, use the median and/or the mode. Instead of the standard deviation, use the mean absolute deviation, the median absolute deviation, or the interquartile range. Instead of z-scores, use percentile ranks. Instead of skewness, use a QQ-plot or a histogram. Instead...","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Unlearning-descriptive-statistics/",
+        "teaser":null},{
+        "title": "SHAttered",
+        "excerpt":"Awesome work to demonstrate how to deliberately cause a SHA-1 collision.      It is now practically possible to craft two colliding PDF files and obtain a SHA-1 digital signature on the first PDF file which can also be abused as a  valid signature on the second PDF file.    (via SHAttered)   ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/SHAttered/",
+        "teaser":null},{
+        "title": "Combating Fake News With a Smartphone",
+        "excerpt":"   Automatically [add] extra digital proof data to all photos and videos you take.    Pretty cool!   (via the Guardian Project)  ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Combating-Fake-News-With-a-Smartphone/",
+        "teaser":null},{
+        "title": "Explaining complex machine learning models with LIME",
+        "excerpt":"Another nice write-up on the use of Local Interpretable Model-Agnostic Explanations (LIME) to explain complex machine learning models.  ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Explaining-complex-machine-learning-models-with-LIME/",
+        "teaser":null},{
+        "title": "Canonical Correlation Analysis for Analyzing Sequences of Medical Billing Codes",
+        "excerpt":"Due to the vast number of medical billing codes, it is generally infeasible to generate machine learning features from them as one-hot vectors. This paper discusses the use of Canonical Correlation Analysis to reduce this dimensionality and capture the inherent relationships that exist between the codes.  ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/CCA-for-Analyzing-Sequences-of-Medical-Billing-Codes/",
+        "teaser":null},{
+        "title": "Gathering weak npm credentials",
+        "excerpt":"We all know the importance of strong passwords, don’t we?   In case you don’t, here’s an example of how a security researcher was able to obtain direct publish access to 14% of npm packages through some fairly basic techniques that take advantage of poor password practices.   (via ChALkeR)  ","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Gathering-weak-npm-credentials/",
+        "teaser":null},{
+        "title": "Using multiple worktrees with git",
+        "excerpt":"When working with multiple branches at the same time, people clone the whole git repository again. I am one of these people. I have been using git for years, and I can’t believe I’ve not known about git worktree! – this makes it easy to work on various braches in...","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Using-multiple-worktrees-with-git/",
+        "teaser":null},{
+        "title": "Are categorical variables getting lost in your random forests?",
+        "excerpt":"I’ve been one-hot encoding categorical variables for as long as I have been using sci-kit learn. It turns out that you can lose a lot of predictive power this way, and that alternatives do exist. Decision tree models can handle categorical variables without one-hot encoding them. However, popular implementations of...","categories": [],
+        "tags": [],
+        "url": "https://proinsias.github.io/pages/proinsias/Are-categorical-variables-getting-lost-in-your-random-forests/",
+        "teaser":null}]
