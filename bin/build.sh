a597ea5af02229007834c6f52278e5380784b722
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2019-09-20 14:20:30 -0400

    Update how we check external links to proinsias.github.io

--- bin/build.sh
+++ bin/build.sh
@@ -12,12 +12,10 @@ set -o xtrace  # Echo commands as they are executed.
 bundle exec jekyll build
 
 bundle exec htmlproofer ./_site \
+    --check-favicon \
     --check-html \
     --empty-alt-ignore `# Unsure how to enable alt tags for teaser images` \
     --http-status-ignore 999 `# Ignore 999 errors associated with LinkedIn urls` \
+    --internal-domains proinsias.github.io `# Treat urls with this domain as internal urls` \
+    --typhoeus-config '{ "connecttimeout": 0, "timeout": 0 }' `# Reset to defaults. Rely on Travis timeouts` \
     --url-ignore "#"
-
-# FIXME: --check-favicon - need to add them first!
-# FIXME: - overcommit --sign - need to edit .overcommit.yml first.
-# FIXME: - overcommit --sign pre-commit
-# FIXME: - overcommit --run
